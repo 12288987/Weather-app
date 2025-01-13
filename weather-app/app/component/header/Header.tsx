82b9a5a718b45b2529b1ea43ccba8de86038608f
Ebrahim Essa Ahmed <99309335+12288987@users.noreply.github.com>
2025-01-13 17:49:42 +0300

    header

---
+++ weather-app/app/component/header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+        <div className='flex justify-between bg-sky-300 h-12 items-center px-6'>
+            <div>Weather</div>
+            <div>Creadit</div>
+        </div>
+    </>
+  )
+}
+
+export default Header
---
+++ weather-app/app/pages/home/HomePage.tsx
@@ -0,0 +1,12 @@
+import Header from '@/app/component/header/Header'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <Header />
+    </div>
+  )
+}
+
+export default HomePage
